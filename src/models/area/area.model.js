e607ce1732ce8e65bc3e5de47768a3df32a304f1
Mahamud <mhbidhan@outlook.com>
2023-04-12 00:35:03 +0600

    Defined model for area

---
+++ src/models/area/area.model.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const areaSchema = new Schema({
+  city: {
+    type: String,
+    min: 3,
+    max: 255,
+    required: true,
+  },
+  name: {
+    type: String,
+    min: 3,
+    max: 255,
+    required: true,
+  },
+  caterers: {
+    type: [],
+    default: [],
+    required: true,
+  },
+});
+
+const areas = model('Area', areaSchema);
+
+module.exports = areas;
