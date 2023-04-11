b66edb57e4f2aff827bf49a7b050c59df6c33836
Mahamud <mhbidhan@outlook.com>
2023-04-12 02:26:11 +0600

    Defined a controller to ger area by id and added it to the router

--- src/controllers/area/area.controller.js
+++ src/controllers/area/area.controller.js
@@ -2,12 +2,29 @@ const areas = require('../../models/area/area.model');
 const areaValidatorSchema = require('./area.validate');
 const validator = require('../../utils/validator');
 const setErrorMessage = require('../../utils/setErrorMessage');
+const errorMessages = require('../../utils/errorMessages');
 
 async function getAllAreas(req, res) {
   try {
     const allAreas = await areas.find({});
 
     return res.status(200).json(allAreas);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+}
+
+async function getAreaById(req, res) {
+  try {
+    const { id } = req.params;
+
+    const area = await areas.findById(id);
+
+    if (!area)
+      return res.status(404).json(setErrorMessage(errorMessages.notFound));
+
+    return res.status(200).json(area);
   } catch (error) {
     console.log(error);
     return res.status(500).json(error);
@@ -25,7 +42,9 @@ async function createNewArea(req, res) {
     const exists = await areas.findOne({ name: body.name });
 
     if (exists)
-      return res.status(400).json(setErrorMessage('Area already exists'));
+      return res
+        .status(400)
+        .json(setErrorMessage(setErrorMessage(errorMessages.exists)));
 
     const newArea = await areas.create(body);
 
@@ -38,5 +57,6 @@ async function createNewArea(req, res) {
 
 module.exports = {
   getAllAreas,
+  getAreaById,
   createNewArea,
 };
--- src/routes/area/area.routes.js
+++ src/routes/area/area.routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const {
   getAllAreas,
   createNewArea,
+  getAreaById,
 } = require('../../controllers/area/area.controller');
 
 const areaRouter = express.Router();
 
 areaRouter.get('/', getAllAreas);
+areaRouter.get('/:id', getAreaById);
 areaRouter.post('/', createNewArea);
 
 module.exports = areaRouter;
