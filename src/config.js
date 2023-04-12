6d59b94644fd57e2d9abfa52b156788b9ca5a541
Mahamud <mhbidhan@outlook.com>
2023-04-13 00:06:05 +0600

    Added environment variables for cloudinary and json web token

--- src/config.js
+++ src/config.js
@@ -2,8 +2,16 @@ require('dotenv').config();
 
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
+const CLOUDINARY_NAME = process.env.CLOUDINARY_NAME;
+const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
+const JWT_KEY = process.env.JWT_KEY;
 
 module.exports = {
   PORT,
   MONGO_URI,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_NAME,
+  CLOUDINARY_API_SECRET,
+  JWT_KEY,
 };
