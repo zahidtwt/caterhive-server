945cfa3dcfea81648587ce2be796b51d06d5b3f3
Mahamud <mhbidhan@outlook.com>
2023-04-15 12:31:46 +0600

    Modified the app to recive larger request buffers

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const { ALLOWED_CORS_DOMAIN } = require('./config');
 const globalRouter = require('./routes/routes');
 
 const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use('/', globalRouter);
 
 module.exports = app;
