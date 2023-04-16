3da68dbe4b91453b6c8e9a22b6035b3d9f5967d8
Mahamud <mhbidhan@outlook.com>
2023-04-16 12:35:51 +0600

    Added new message to the error message module

--- src/utils/errorMessages.js
+++ src/utils/errorMessages.js
@@ -3,6 +3,7 @@ const errorMessages = {
   notFound: 'Data not found',
   invlidLogin: 'The email and password does not match',
   accessDenied: 'Access Denied',
+  invalidUser: 'The user is not allowed to perform this action',
 };
 
 module.exports = errorMessages;
