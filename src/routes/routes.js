99c12fc0b5834f3d978332f4ae8ae336beb6acb4
Your Name <you@example.com>
2023-04-17 15:04:48 +0600

    Defined day menu controllers and routes and added them to the main app

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,7 @@ const catererRouter = require('./caterer/caterer.routes');
 const customerRouter = require('./customer/customer.routes');
 const foodItemsRouter = require('./food-item/food-item.routes');
 const menusRouter = require('./menu/menu.routes');
+const dayMenuRouter = require('./day-menu/day-menu.routes');
 
 const globalRouter = express.Router();
 
@@ -12,5 +13,6 @@ globalRouter.use('/caterers', catererRouter);
 globalRouter.use('/customers', customerRouter);
 globalRouter.use('/foodItems', foodItemsRouter);
 globalRouter.use('/menus', menusRouter);
+globalRouter.use('/dayMenus', dayMenuRouter);
 
 module.exports = globalRouter;
