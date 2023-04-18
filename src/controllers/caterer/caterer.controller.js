8e3095ea5329a6192a16c927d392ea587f2a6680
Your Name <you@example.com>
2023-04-18 21:44:31 +0600

    Modified the caterer controller to fix the bug that was coliding with the data schema

--- src/controllers/caterer/caterer.controller.js
+++ src/controllers/caterer/caterer.controller.js
@@ -227,13 +227,13 @@ async function addDayMenu(req, res) {
 
     if (error) return res.status(400).json(error.message);
 
-    caterer.weekMenu[body.day] = body.dayMenu;
+    caterer.weekMenu[body.day.toLowerCase()] = body.dayMenu;
 
     await caterer.save();
 
     await caterer.populate(weekMenuPopulation);
 
-    return res.status(200).json(caterer);
+    return res.status(200).json(caterer.weekMenu);
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
