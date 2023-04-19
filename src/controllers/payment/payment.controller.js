0db06c3320bca5618ca0264492a02baf077c3809
Mahamud <mhbidhan@outlook.com>
2023-04-19 16:50:29 +0600

    Changed the url for payment gataway

--- src/controllers/payment/payment.controller.js
+++ src/controllers/payment/payment.controller.js
@@ -10,16 +10,38 @@ async function makePayment(req, res) {
     const data = {
       total_amount: 100,
       currency: 'BDT',
-      order_id: 'REF123', // use unique tran_id for each api call
-      success_url: 'http://localhost:4000/paymnet/success',
-      fail_url: 'http://localhost:4000/paymnet/fail',
-      cancel_url: 'http://localhost:4000/paymnet/cancel',
-      ipn_url: 'http://localhost:4000/paymnet/ipn',
+      tran_id: 'REF123', // use unique tran_id for each api call
+      success_url: 'https://caterhive-server.onrender.com/success',
+      fail_url: 'https://caterhive-server.onrender.com/fail',
+      cancel_url: 'https://caterhive-server.onrender.com/cancel',
+      ipn_url: 'https://caterhive-server.onrender.com/ipn',
+      shipping_method: 'Courier',
+      product_name: 'Computer.',
+      product_category: 'Electronic',
+      product_profile: 'general',
+      cus_name: 'Customer Name',
+      cus_email: 'customer@example.com',
+      cus_add1: 'Dhaka',
+      cus_add2: 'Dhaka',
+      cus_city: 'Dhaka',
+      cus_state: 'Dhaka',
+      cus_postcode: '1000',
+      cus_country: 'Bangladesh',
+      cus_phone: '01711111111',
+      cus_fax: '01711111111',
+      ship_name: 'Customer Name',
+      ship_add1: 'Dhaka',
+      ship_add2: 'Dhaka',
+      ship_city: 'Dhaka',
+      ship_state: 'Dhaka',
+      ship_postcode: 1000,
+      ship_country: 'Bangladesh',
     };
     const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
     sslcz.init(data).then((apiResponse) => {
       // Redirect the user to payment gateway
       let GatewayPageURL = apiResponse.GatewayPageURL;
+
       res.redirect(GatewayPageURL);
       console.log('Redirecting to: ', GatewayPageURL);
     });
