c636f6f533b7a543d76ea2648f55287d6107fd2f
Mahamud <mahamud@gamil.com>
2023-04-26 21:19:52 +0600

    Defined event-service-order model and it's validator

---
+++ src/controllers/event-service-order/event-service-order.validator.js
@@ -0,0 +1,17 @@
+const joi = require('joi');
+
+const eventServiceOrderValidatorSchema = joi.object({
+  caterer: joi.string().min(24).max(24).required().label('Caterer'),
+  menu: joi.string().min(24).max(24).required().label('Menu'),
+  quantity: joi.number().min(1).required().label('Quantity'),
+  orderValue: joi.number().required().label('Order Value'),
+  shippingAddress: joi
+    .string()
+    .min(3)
+    .max(255)
+    .required()
+    .label('Shipping Address'),
+  specialInstruction: joi.string().max(255).label('Special Instruction'),
+});
+
+module.exports = eventServiceOrderValidatorSchema;
---
+++ src/models/event-sevice-order/event-service-order.model.js
@@ -0,0 +1,62 @@
+const { Schema, model } = require('mongoose');
+
+const eventServiceOrderShcema = new Schema({
+  caterer: {
+    type: Schema.Types.ObjectId,
+    ref: 'Caterer',
+    required: true,
+  },
+  customer: {
+    type: Schema.Types.ObjectId,
+    ref: 'Customer',
+    required: true,
+  },
+  menu: {
+    type: Schema.Types.ObjectId,
+    ref: 'Event-Menu',
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    min: 1,
+    required: true,
+  },
+  orderValue: {
+    type: Number,
+    required: true,
+  },
+  orderStatus: {
+    type: String,
+    min: 3,
+    max: 255,
+    required: true,
+  },
+  paymentDue: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  shippingAddress: {
+    type: String,
+    min: 3,
+    max: 255,
+    required: true,
+  },
+  specialInstruction: {
+    type: String,
+    min: 3,
+    max: 255,
+  },
+  orderedAt: {
+    type: Date,
+    required: true,
+    default: new Date().toISOString(),
+  },
+});
+
+const eventServiceOrders = model(
+  'Event-Service-Order',
+  eventServiceOrderShcema
+);
+
+module.exports = eventServiceOrders;
