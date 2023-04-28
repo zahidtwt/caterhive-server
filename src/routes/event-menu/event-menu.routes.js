6a9b71961886655cb7d39db0893d49e35a8f97ff
Mahamud <mhbidhan@outlook.com>
2023-04-28 19:36:28 +0600

    Modified the event menu router to add the new controller

--- src/routes/event-menu/event-menu.routes.js
+++ src/routes/event-menu/event-menu.routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const auth = require('../../middlewares/auth');
 const {
   getAllEventMenusByCaterer,
+  getEventMenusById,
   createNewEventMenu,
 } = require('../../controllers/event-menu/event-menu.contrroller');
 
 const eventMenuRouter = express.Router();
 
 eventMenuRouter.get('/', auth, getAllEventMenusByCaterer);
+eventMenuRouter.get('/:id', auth, getEventMenusById);
 eventMenuRouter.post('/', auth, createNewEventMenu);
 
 module.exports = eventMenuRouter;
